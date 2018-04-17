4f48ca77efe9b7b7c1accd22ded3c4eedaae96ce
lizhipeng@bwton.com <lizhipeng@bwton.com>
2018-04-17 22:09:11 +0800

    注释

--- springcloud-eureka-product/src/main/java/com/lizp/springcloud/controller/IndexController.java
+++ springcloud-eureka-product/src/main/java/com/lizp/springcloud/controller/IndexController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.lizp.springcloud.service.TestService;
 
-@RefreshScope
+@RefreshScope // 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中
 @RestController
 @RequestMapping(value = "/product")
 public class IndexController {
