c6313fef3ce506396d7f568e9795eee4dd1d1522
lizhipeng@bwton.com <lizhipeng@bwton.com>
2018-04-17 09:10:54 +0800

    优化

--- springcloud-eureka-product/src/main/java/com/lizp/springcloud/service/TestService.java
+++ springcloud-eureka-product/src/main/java/com/lizp/springcloud/service/TestService.java
@@ -3,15 +3,20 @@ package com.lizp.springcloud.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TestService {
+	@Value("${server.port}")
+	private String port;
+
 	public List<String> getTest() {
 		List<String> list = new ArrayList<String>();
 		list.add("sfsdf");
 		list.add("346534");
 		list.add("gjghjkgh");
+		list.add(port);
 		return list;
 	}
 }
