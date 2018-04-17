6022fa372c7f7205a7a8e1f2206a33fef78305fe
lizhipeng@bwton.com <lizhipeng@bwton.com>
2018-04-17 11:13:19 +0800

    配置

--- springcloud-eureka-product/src/main/java/com/lizp/springcloud/controller/IndexController.java
+++ springcloud-eureka-product/src/main/java/com/lizp/springcloud/controller/IndexController.java
@@ -3,22 +3,38 @@ package com.lizp.springcloud.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.ModelMap;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.config.environment.Environment;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lizp.springcloud.service.TestService;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/product")
 public class IndexController {
 	@Autowired
 	private TestService testService;
+	@Value("${sang}")
+	private String sang;
+//	@Autowired
+//	private Environment env;
+
+	@RequestMapping("/sang")
+	public String sang() {
+		return this.sang;
+	}
+
+//	@RequestMapping("/sang2")
+//	public Environment sang2() {
+//		return env;
+//	}
 
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
-	public String index(ModelMap map) {
-		map.put("title", "欢迎");
+	public String index() {
 		return "index";
 	}
 
