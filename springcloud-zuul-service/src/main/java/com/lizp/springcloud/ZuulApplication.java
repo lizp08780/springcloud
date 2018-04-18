fe3b1901c0819064ce55a2d026cdde5a4ac7ccdd
lizhipeng@bwton.com <lizhipeng@bwton.com>
2018-04-18 11:33:57 +0800

    测试

--- springcloud-zuul-service/src/main/java/com/lizp/springcloud/ZuulApplication.java
+++ springcloud-zuul-service/src/main/java/com/lizp/springcloud/ZuulApplication.java
@@ -2,8 +2,10 @@ package com.lizp.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableDiscoveryClient
 @EnableZuulProxy // 开启 Zuul 服务网关
 @SpringBootApplication
 public class ZuulApplication {
